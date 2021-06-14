1946e8f70370b090ca121a6d94439ebbc20f7a8b
Satomi Ichii <sichii@gmail.com>
2021-06-14 16:14:17 -0400

    feat: infinity scroll

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,23 +1,15 @@
-import React from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 import { Pins } from './Pins';
+import { Footer } from './Footer';
 import data from '../../public/nyc_ttp_pins.json';
 
-export default class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data,
-    };
-  }
-
-  render() {
-    console.log(data);
-    return (
-      <div>
-        <div>Home Component Rendered</div>
-        <Pins />
-      </div>
-    );
-  }
-}
+export const Home = (props) => {
+  const [pins, setPins] = useState(data);
+  return (
+    <div>
+      <div>Home Component Rendered</div>
+      <Pins pins={pins} />
+      <Footer />
+    </div>
+  );
+};
--- app/components/Pins.js
+++ app/components/Pins.js
@@ -1,5 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 
 export const Pins = (props) => {
-  return <div>Pins Rendered</div>;
+  const [listItem, setListItem] = useState([]);
+  const [page, setPage] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const perPage = 8;
+  const maxPage = Math.ceil(props.pins.length / perPage);
+
+  const observer = useRef();
+  const lastItemRef = useCallback(
+    (elm) => {
+      if (!loading) {
+        if (observer.current) {
+          observer.current.disconnect();
+        }
+        observer.current = new IntersectionObserver((entries) => {
+          if (entries[0].isIntersecting) {
+            console.log(listItem.length);
+            if (page >= maxPage) {
+              setPage(0);
+            } else {
+              setPage((prev) => prev + 1);
+            }
+          }
+        });
+        if (elm) {
+          observer.current.observe(elm);
+        }
+        console.log('Last');
+      }
+    },
+    [loading, page, maxPage]
+  );
+
+  useEffect(() => {
+    setLoading(true);
+    console.log('page', page);
+    const start = page * perPage;
+    const end = start + perPage;
+    const newItems = props.pins.slice(start, end);
+    setListItem((prev) => {
+      return [...prev, ...newItems];
+    });
+    setLoading(false);
+  }, [page]);
+
+  return (
+    <div>
+      {listItem.map((pin, idx) => {
+        return (
+          (idx === listItem.length - 1 && (
+            <div ref={lastItemRef} key={pin.board.id}>
+              <img src={pin.images['136x136'].url} />
+            </div>
+          )) || (
+            <div key={pin.board.id}>
+              <img src={pin.images['136x136'].url} />
+            </div>
+          )
+        );
+      })}
+      {loading && <div>LOADING...</div>}
+    </div>
+  );
 };
